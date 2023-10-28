4517b90a4d810ddd1eb8463cd61ec7db9294cacc
Jimmy Nguyen <jimmyn6565@gmail.com>
2023-10-27 22:46:53 -0700

    Removed unnecessary lines

--- employee.h
+++ employee.h
@@ -8,9 +8,3 @@ typedef struct {
 } Employee, *PtrToEmployee;
 
 typedef const Employee *PtrToConstEmployee;
-
-PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number);
-PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char *name);
-PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char *phone);
-PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary);
-
